615eed594c99c66ee8b3abf6348fbb8d0e002b2f
Ryan Rilly Tirkey <103141303+ryantirkey@users.noreply.github.com>
2025-07-04 19:54:14 +0530

    Add files via upload

---
+++ springBoot_MVC_ChangeEmbededServer_28/src/main/java/com/ty/SpringBootMvcChangeEmbededServer28Application.java
@@ -0,0 +1,13 @@
+package com.ty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcChangeEmbededServer28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcChangeEmbededServer28Application.class, args);
+	}
+
+}
